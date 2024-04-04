90a3efe0fb807802fee380ec7325a5243b37337c
Arseny Buravlev <arseny.buravlyov@gmail.com>
2024-04-04 19:33:57 +0400

    Add getNeighborsBase32 and getNeighborsInt to exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export { Coordinates, HashInt, Bbox, Direction, Neighbors, GeoJsonFeature } from './types';
 export { encodeInt, decodeInt, encodeBase32, decodeBase32 } from './hashing';
-export { getNeighborInt, getNeighborBase32 } from './neighbors';
+export {
+  getNeighborInt,
+  getNeighborBase32,
+  getNeighborsInt,
+  getNeighborsBase32,
+} from './neighbors';
 export {
   encodeBboxInt,
   decodeBboxInt,
